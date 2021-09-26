227ac7febfc0ec5757d890c93859d8fdbd481a20
Marco Esposito <esposito@imfusion.com>
2021-09-26 16:42:35 +0200

    Publishing works

--- easy_handeye2/easy_handeye2/handeye_publisher.py
+++ easy_handeye2/easy_handeye2/handeye_publisher.py
@@ -3,58 +3,37 @@
 import rclpy
 import tf2_ros
 import geometry_msgs.msg
-from easy_handeye2.handeye_calibration import HandeyeCalibration
+from easy_handeye2.handeye_calibration import load_calibration
 
 
 class HandeyePublisher(rclpy.node.Node):
     def __init__(self):
         super().__init__('handeye_publisher')
 
-        self.declare_parameter('inverse', False)
-        self.declare_parameter('calibration_file')
+        self.declare_parameter('name')
+        name = self.get_parameter('name').get_parameter_value().string_value
 
-        inverse = self.get_parameter('inverse').get_parameter_value().bool_value  # TODO implement or remove
-        filename = self.get_parameter('calibration_file').get_parameter_value().string_value
+        self.get_logger().info(f'Loading the calibration with name {name}')
 
-        if filename == '':
-            self.get_logger().debug('No path specified for the calibration file, loading from the standard location')
-            filename = HandeyeCalibration.filename_for_namespace(self.get_namespace())
+        self.calibration = load_calibration(name)
+        parameters = self.calibration.parameters
 
-        self.get_logger().info("Loading the calibration from file: %s", filename)
-        calib = HandeyeCalibration.from_filename(filename)
-
-        if calib.parameters.eye_in_hand:
-            self.declare_parameter('robot_effector_frame')
-            overriding_robot_effector_frame = self.get_parameter('robot_effector_frame').get_parameter_value().string_value
-            if overriding_robot_effector_frame != "":
-                calib.transformation.header.frame_id = overriding_robot_effector_frame
+        if parameters.eye_in_hand:
+            orig = parameters.robot_effector_frame
         else:
-            self.declare_parameter('robot_base_frame')
-            overriding_robot_base_frame = self.get_parameter('robot_base_frame').get_parameter_value().string_value
-            if overriding_robot_base_frame != "":
-                calib.transformation.header.frame_id = overriding_robot_base_frame
-        self.declare_parameter('tracking_base_frame')
-        overriding_tracking_base_frame = self.get_parameter('tracking_base_frame').get_parameter_value().string_value
-        if overriding_tracking_base_frame != "":
-            calib.transformation.child_frame_id = overriding_tracking_base_frame
+            orig = parameters.robot_base_frame
+        dest = parameters.tracking_base_frame
 
-        self.get_logger().info('loading calibration parameters into namespace {}'.format(
-            self.get_logger().get_namespace()))
-        HandeyeCalibration.store_to_parameter_server(self, calib)  # TODO implement
+        self.broadcaster = tf2_ros.StaticTransformBroadcaster(self)
+        self.static_transformStamped = geometry_msgs.msg.TransformStamped()
 
-        orig = calib.transformation.header.frame_id  # tool or base link
-        dest = calib.transformation.child_frame_id  # tracking_base_frame
+        self.static_transformStamped.header.stamp = self.get_clock().now().to_msg()
+        self.static_transformStamped.header.frame_id = orig
+        self.static_transformStamped.child_frame_id = dest
 
-        broadcaster = tf2_ros.StaticTransformBroadcaster(self)
-        static_transformStamped = geometry_msgs.msg.TransformStamped()
+        self.static_transformStamped.transform = self.calibration.transform
 
-        static_transformStamped.header.stamp = self.get_clock().now()
-        static_transformStamped.header.frame_id = orig
-        static_transformStamped.child_frame_id = dest
-
-        static_transformStamped.transform = calib.transformation.transform
-
-        broadcaster.sendTransform(static_transformStamped)
+        self.broadcaster.sendTransform(self.static_transformStamped)
 
 
 def main(args=None):
---
+++ easy_handeye2/launch/publish.launch.py
@@ -0,0 +1,18 @@
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.conditions import IfCondition, UnlessCondition
+from launch.substitutions import LaunchConfiguration, PythonExpression
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+    arg_name = DeclareLaunchArgument('name')
+
+    handeye_publisher = Node(package='easy_handeye2', executable='handeye_publisher', name='handeye_publisher', parameters=[{
+        'name': LaunchConfiguration('name'),
+    }])
+
+    return LaunchDescription([
+        arg_name,
+        handeye_publisher,
+    ])
